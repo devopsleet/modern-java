c008ef1eb5d44ad5b66b94034469cbc3cabc9ea0
gagan <gasingla07@gmail.com>
2025-01-13 22:09:00 -0600

    Operators

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        double variable1 = 20.00;
+        double variable2 = 80.00;
+
+        double result = variable1 + variable2 * 100.00;
+        System.out.println("Total is " + result);
+        double remainder = result % 40.00;
+
+        boolean var = (remainder == 0.00) ? true : false;
+
+        System.out.println("Output is " + var);
     }
+
 }
