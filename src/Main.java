b6e7b9ec65f49768a5e8fe478b66cbfb53aabd88
gagan <gasingla07@gmail.com>
2025-02-04 16:58:21 -0600

    More on Java methods

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,66 @@
-
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("The highScore is " + highScore);
+        //int myIntMaxValue = Integer.MAX_VALUE +1;
+        int myIntMaxValue = 2147483647;
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        System.out.println("Busted Max Value = " + myIntMaxValue);
 
-        System.out.println("The next highScore is " + calculateScore(gameOver, score, levelCompleted, bonus));
-
-
+//        int myFirstNumber = 10;
+//
+//        myFirstNumber = 23;
+//
+//        System.out.println(myFirstNumber);
 //        boolean gameOver = true;
 //        int score = 800;
 //        int levelCompleted = 5;
 //        int bonus = 100;
+//
+//        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//        System.out.println("The highScore is " + highScore);
+//
+//        score = 10000;
+//        levelCompleted = 8;
+//        bonus = 200;
+//
+//        System.out.println("The next highScore is " + calculateScore(gameOver, score, levelCompleted, bonus));
+//
+//
+////        boolean gameOver = true;
+////        int score = 800;
+////        int levelCompleted = 5;
+////        int bonus = 100;
+////
+////        int finalScore = score;
+////
+////        if (gameOver) {
+////            finalScore += (levelCompleted * bonus);
+////            finalScore += 1000;
+////            System.out.println("Your final score was" + finalScore);
+////        }
+//
+//
+//    }
+//
+//    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//
+////        boolean gameOver = true;
+////        int score = 800;
+////        int levelCompleted = 5;
+////        int bonus = 100;
 //
 //        int finalScore = score;
 //
 //        if (gameOver) {
 //            finalScore += (levelCompleted * bonus);
 //            finalScore += 1000;
-//            System.out.println("Your final score was" + finalScore);
+////            System.out.println("Your final score was " + finalScore);
 //        }
-
-
-    }
-
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-
-//        boolean gameOver = true;
-//        int score = 800;
-//        int levelCompleted = 5;
-//        int bonus = 100;
-
-        int finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-//            System.out.println("Your final score was " + finalScore);
-        }
-
-        return finalScore;
-
-    }
+//
+//        return finalScore;
+//
+   }
 
 }
