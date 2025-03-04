c5f84fca00d80d72c117c11246f858e7fea43486
gagan <gasingla07@gmail.com>
2025-03-04 14:18:27 -0600

    oop with getter and setter

--- src/Main.java
+++ src/Main.java
@@ -3,65 +3,13 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        //int myIntMaxValue = Integer.MAX_VALUE +1;
-        int myIntMaxValue = 2147483647;
-
-        System.out.println("Busted Max Value = " + myIntMaxValue);
-        System.out.println("Hello, " + "Gagan");
-
-//        int myFirstNumber = 10;
-//
-//        myFirstNumber = 23;
-//
-//        System.out.println(myFirstNumber);
-//        boolean gameOver = true;
-//        int score = 800;
-//        int levelCompleted = 5;
-//        int bonus = 100;
-//
-//        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-//        System.out.println("The highScore is " + highScore);
-//
-//        score = 10000;
-//        levelCompleted = 8;
-//        bonus = 200;
-//
-//        System.out.println("The next highScore is " + calculateScore(gameOver, score, levelCompleted, bonus));
-//
-//
-////        boolean gameOver = true;
-////        int score = 800;
-////        int levelCompleted = 5;
-////        int bonus = 100;
-////
-////        int finalScore = score;
-////
-////        if (gameOver) {
-////            finalScore += (levelCompleted * bonus);
-////            finalScore += 1000;
-////            System.out.println("Your final score was" + finalScore);
-////        }
-//
-//
-//    }
-//
-//    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-//
-////        boolean gameOver = true;
-////        int score = 800;
-////        int levelCompleted = 5;
-////        int bonus = 100;
-//
-//        int finalScore = score;
-//
-//        if (gameOver) {
-//            finalScore += (levelCompleted * bonus);
-//            finalScore += 1000;
-////            System.out.println("Your final score was " + finalScore);
-//        }
-//
-//        return finalScore;
-//
-   }
+        Car car = new Car();
+        car.setMake("holden");
+        car.setModel("Carrera");
+        car.setDoors(4);
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        System.out.println("Doors = " + car.getDoors());
+    }
 
 }
