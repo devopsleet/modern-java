adaaf6db797b3e01f5e4be3f12abc40c9c807e7b
gagan <gasingla07@gmail.com>
2025-04-01 16:09:43 -0500

    String array

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -9,5 +9,11 @@ public class StringDemo {
         System.out.println("Char at index 3 in strOb1 is " + strOb1.charAt(3));
 
         if(strOb1.equals(strOb3)) System.out.println("Both the objects are equal");
+
+        String[]  str = {"One", "Two", "Three"};
+
+        for(String s : str){
+            System.out.println(s);
+        }
     }
 }
