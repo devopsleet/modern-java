3cb7b088813b43be56998d6f2477c231fd3b2f52
gagan <gasingla07@gmail.com>
2025-04-09 12:07:27 -0500

    packages and exception handling

---
+++ src/Exc0.java
@@ -0,0 +1,14 @@
+public class Exc0 {
+    static void subroutine() {
+
+        int d = 0;
+        int a = 42 / d;
+
+    }
+
+    public static void main(String[] args) {
+
+        Exc0.subroutine();
+
+    }
+}
---
+++ src/p2/OtherPackage.java
@@ -0,0 +1,5 @@
+package p2;
+
+class OtherPackage {
+    p1.Protection p = new p1.Protection();
+}
