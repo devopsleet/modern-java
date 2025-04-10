890dadc7e5bbeebd043f230ee98e8841e5af622c
gagan <gasingla07@gmail.com>
2025-04-10 12:31:53 -0500

    example progarm on throws

--- src/ThrowDemo.java
+++ src/ThrowDemo.java
@@ -1,18 +1,30 @@
+import java.nio.channels.FileLockInterruptionException;
+
 class ThrowDemo {
     static void demoproc() {
         try {
             throw new NullPointerException("demo");
-        }catch(NullPointerException e) {
+        } catch (NullPointerException e) {
             System.out.println("Cauught Inside demoproc");
-            throw e;
+            //throw e;
         }
     }
+
+    static void throwOne() throws IllegalAccessException{
+
+            System.out.println("Inside ThrowOne");
+            throw new IllegalAccessException("demo");
+    }
+
 
     public static void main(String[] args) {
         try{
             demoproc();
+            throwOne();
         } catch (NullPointerException e) {
-            System.out.println("Recaught :" + e);
+            System.out.println("Recaught :" + e.getMessage());
+        } catch (IllegalAccessException e){
+            System.out.println(e.getMessage());
         }
     }
 }
