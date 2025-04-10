34de50b1b423d9a349415f852ef231ab9ae90e72
gagan <gasingla07@gmail.com>
2025-04-10 12:06:20 -0500

    throw and rethrow an exception program

---
+++ src/ThrowDemo.java
@@ -0,0 +1,18 @@
+class ThrowDemo {
+    static void demoproc() {
+        try {
+            throw new NullPointerException("demo");
+        }catch(NullPointerException e) {
+            System.out.println("Cauught Inside demoproc");
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        try{
+            demoproc();
+        } catch (NullPointerException e) {
+            System.out.println("Recaught :" + e);
+        }
+    }
+}
