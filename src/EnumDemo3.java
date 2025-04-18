4f749bcf83aef089c42b2a89fd3e92dcd8d45ca1
gagan <gasingla07@gmail.com>
2025-04-18 12:38:43 -0500

    autoboxing

--- src/EnumDemo3.java
+++ src/EnumDemo3.java
@@ -1,31 +1,24 @@
 enum Apple1 {
-    Jonathan(10), GoldenDel, RedDel(12);
+    Jonathan(10), RedDel(12);
 
     private int price;
 
     Apple1(int p) {
+
+        System.out.println(p);
         price = p;
     }
 
-    Apple1() {
-        price = -1;
-    }
-
-    int getPrice() {
-        return price;
-    }
 }
 
 public class EnumDemo3 {
     public static void main(String[] args) {
         Apple1 ap;
 
-        //Display the price of Winesap.
-        System.out.println(Apple.GoldenDel.getPrice());
+        for(Apple1 apples: Apple1.values()) {
 
-        for(Apple1 a: Apple1.values()) {
-            System.out.println(a.getPrice());
         }
+
     }
 
 
