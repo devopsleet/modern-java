b1371b36064a6b69b65a30f3dcdf7733058ab3df
gagan <gasingla07@gmail.com>
2025-04-29 13:51:59 -0500

    automatic type promotion

--- src/Cast.java
+++ src/Cast.java
@@ -1,11 +1,17 @@
 public class Cast {
     public static void main(String[] args) {
-        int a = 129;
-        byte b;
 
-        b = (byte) a;
+        byte b = 50;
+
+        b = (byte) (b * 2);
 
         System.out.println(b);
+//        int a = 129;
+//        byte b;
+//
+//        b = (byte) a;
+//
+//        System.out.println(b);
     }
 
 }
