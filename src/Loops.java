730c0238f0f7f9f82227769d72009d41697cdb20
gagan <gasingla07@gmail.com>
2025-04-30 11:43:04 -0500

    enhanced for-loop

---
+++ src/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int x: nums) {
+            sum = sum + x;
+
+        }
+        System.out.println(sum);
+    }
+
+}
