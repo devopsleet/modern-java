41f7157811d0f6042f972b2217325c0f6fba7f42
gagan <gasingla07@gmail.com>
2025-05-01 13:13:19 -0500

    generic class with two typ parameters

---
+++ src/SimpGen.java
@@ -0,0 +1,29 @@
+class TwoGen<T,V> {
+    T ob1;
+    V ob2;
+
+    TwoGen(T o, V v) {
+        ob1 = o;
+        ob2 = v;
+    }
+
+    T getObj1() {
+        return ob1;
+    }
+
+    V getObj2() {
+        return ob2;
+    }
+
+}
+public class SimpGen {
+    public static void main(String[] args) {
+        TwoGen<Integer, String> tgObj= new TwoGen<Integer, String>(88, "test");
+
+        int v = tgObj.getObj1();
+        String s = tgObj.getObj2();
+
+        System.out.println(v);
+        System.out.println(s);
+    }
+}
