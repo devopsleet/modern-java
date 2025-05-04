b4dc1a6470dd425c0662ab757fa04031645301b4
gagan <gasingla07@gmail.com>
2025-05-04 18:11:37 -0500

    sort by parity

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -36,7 +36,24 @@ public class Main {
             System.out.println("ObjectVariable is really an int array");
         }
 
-        Object[] objectArray =
 
+
+    }
+}
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int[] ans = new int[A.length];
+        int t = 0;
+
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 0)
+                ans[t++] = A[i];
+
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 1)
+                ans[t++] = A[i];
+
+        return ans;
     }
 }
