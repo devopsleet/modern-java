a984aa55737ba5f2569be039bee956ccf4b602d7
gagan <gasingla07@gmail.com>
2025-05-10 13:01:07 -0500

    data types in Java

---
+++ src/Practice.java
@@ -0,0 +1,30 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        int myMaxIntTest = 2_147_18;
+        System.out.println(myMaxIntTest);
+
+        System.out.println("Byte Value range is from " + Byte.MIN_VALUE  + " to " + Byte.MAX_VALUE);
+        System.out.println("Byte Value range is from " + Short.MIN_VALUE  + " to " + Short.MAX_VALUE);
+        System.out.println(Integer.SIZE);
+
+        long myLongValue = 100L;
+        System.out.println(myLongValue);
+
+        System.out.println("A long has a width of " + Long.SIZE);
+        System.out.println(Long.MAX_VALUE);
+
+        long myLongVal = (long) Integer.MAX_VALUE + 2;
+        System.out.println(myLongVal);
+
+        short myMinShortValue = Short.MIN_VALUE; int myIntVal = Integer.MIN_VALUE;
+        byte myMinByteValue =
+
+    }
+    //int myMaxIntTest = 2_147_18;
+
+
+
+}
