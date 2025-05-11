dddcc2f780d033938b6cfe8b98e1c85ac9d6e23d
gagan <gasingla07@gmail.com>
2025-05-11 12:22:01 -0500

    data types in Java

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,4 @@
-import org.w3c.dom.ls.LSOutput;
+
 
 public class Practice {
 
@@ -20,7 +20,49 @@ public class Practice {
         System.out.println(myLongVal);
 
         short myMinShortValue = Short.MIN_VALUE; int myIntVal = Integer.MIN_VALUE;
-        byte myMinByteValue =
+        byte myMinByteValue = Byte.MIN_VALUE, myMaxByteValue = Byte.MAX_VALUE;
+
+        //byte myMinByteValue = Byte.MIN_VALUE;
+
+        //byte myTotal = (myMinByteValue / 2);
+        //System.out.println(myTotal);
+
+        byte a = Byte.MAX_VALUE;
+
+        short s = Short.MAX_VALUE;
+
+        int i = Integer.MAX_VALUE;
+
+        long l = (50000L + 10L * ((long)a + s + i));
+        System.out.println(l);
+
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        float myFloatValue = 5;
+        System.out.println(myFloatValue);
+
+        float myOtherFloatValue = (float) 5.25;
+        System.out.println(myOtherFloatValue);
+
+        int myIntValue = 5;
+       // float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        myFloatValue  = 5.0f /2;
+        System.out.println(myFloatValue);
+
+        float pounds = 200f;
+        float kilos = pounds * .45359237f;
+        System.out.println(kilos);
+
+        char myChar = 'D';
+        System.out.println(myChar);
+
+
+
 
     }
     //int myMaxIntTest = 2_147_18;
