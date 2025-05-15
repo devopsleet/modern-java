fe50805a65247f219262182a94588c085246bd43
gagan <gasingla07@gmail.com>
2025-05-15 15:58:16 -0500

    nested and inner class

--- src/InnerClassDemo.java
+++ src/InnerClassDemo.java
@@ -2,21 +2,23 @@ class Outer {
     int outer_x = 100;
 
     void test() {
-        Inner inner = new Inner();
-        inner.display();
+        Inner in = new Inner();
+        in.display();
     }
 
     class Inner {
+        int y = 10;
+
+
         void display() {
-            System.out.println("The value of outer x = " + outer_x);
+            System.out.println("The value of outer x is " + outer_x);
         }
     }
 }
 
-public class InnerClassDemo {
+class InnerClassDemo {
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        outer.test();
+        Outer o = new Outer();
+        o.test();
     }
-
 }
