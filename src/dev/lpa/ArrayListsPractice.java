b1150b74858b78f2f64d1088c39d61120c4667a3
gagan <gasingla07@gmail.com>
2025-05-21 14:52:31 -0500

    utils | Arrays

--- src/dev/lpa/ArrayListsPractice.java
+++ src/dev/lpa/ArrayListsPractice.java
@@ -1,7 +1,26 @@
 package dev.lpa;
 
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+class ArrayTest {
+
+}
 public class ArrayListsPractice {
     public static void main(String[] args) {
+
+        ArrayTest at = new ArrayTest();
+        System.out.println(at.getClass());
+        System.out.println(at.hashCode());
+
+        int[] numbers = new int[10];
+        System.out.println(numbers.getClass().getName());
+        System.out.println(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
