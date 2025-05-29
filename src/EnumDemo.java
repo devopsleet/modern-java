16e30cc2d88b9a98967b5f0f9433147f93d899f1
gagan <gasingla07@gmail.com>
2025-05-29 15:08:39 -0500

    enum

--- src/EnumDemo.java
+++ src/EnumDemo.java
@@ -1,62 +1,30 @@
-// An enumeration of apple varities
-
 enum Apple {
-    Jonathan(10), GoldenDel(9), RedDel(12), Winesap(15), Cortland(8);
-
-    private int price; // price of each apple
-
-    Apple(int p) {
-        price = p;
-    }
-
-    public int getPrice() {
-        return price;
-    }
+    Jonathan, GoldenDel, RedDel, Winesap, Cortland
 }
 
-public class EnumDemo {
-
-
+class EnumDemo {
     public static void main(String[] args) {
         Apple ap;
 
         ap = Apple.RedDel;
 
-        // Output an enum value.
-        System.out.println("Value of ap: " + ap);
+        System.out.println("Value of ap " + ap);
         System.out.println();
 
-        ap = Apple.Winesap;
+        ap = Apple.GoldenDel;
 
-        // Compare two enum values
-        if (ap == Apple.Winesap) {
-            System.out.println("ap contains GoldenDel \n");
-        }
+        if (ap == Apple.GoldenDel)
+            System.out.println("ap contains GoldenDel");
 
-        switch(ap) {
-            case Jonathan:
-                System.out.println("Hey Jonathan");
-                break;
-            case Winesap:
-                System.out.println("Hey Winesap");
-                break;
-        }
+        String result = switch(ap) {
+            case Jonathan -> { yield "Jonathan is red" ;}
+            case GoldenDel -> { yield "Golden Del is yellow";}
 
-        System.out.println("Here are all apple constants");
+            default -> { yield "default";}
 
-        Apple[] allapples = Apple.values();
-        for(Apple a: Apple.values())
-            System.out.println(a);
 
-        System.out.println();
+        };
 
-        // Display the prices
-        System.out.println(Apple.Winesap.getPrice());
-
-        System.out.println("All Apple Prices");
-        for(Apple a : Apple.values()) {
-            System.out.println(a + " costs " + a.getPrice() + " cents.");
-        }
-
+        System.out.println(result);
     }
 }
