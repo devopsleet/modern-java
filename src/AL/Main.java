64fa1d27f1dc7442cc566d600c55ec04ee9e16bd
gagan <gasingla07@gmail.com>
2025-06-03 13:38:40 -0500

    composition

---
+++ src/AL/Main.java
@@ -0,0 +1,8 @@
+package AL;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/CF01/Main.java
+++ src/CF01/Main.java
@@ -1,4 +1,18 @@
 package CF01;
 
+//import CF01.PersonalComputer;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ComputerCase theNewCase = new ComputerCase("2208", "Dell", "240");
+        Monitor theNewMonitor = new Monitor("27inch Beast", "Acer", 27, "2540 x 1440");
+        Motherboard theNewMotherboard = new Motherboard("BJ-200","Asus", 4,6,"v2.44");
+        PersonalComputer theNewPc = new PersonalComputer("2208", "Dell", theNewCase, theNewMonitor, theNewMotherboard);
+
+        theNewPc.getMonitor().drawPixelAt(10,10,"red");
+        theNewPc.getMotherboard().loadProgram("Windows OS");
+        theNewPc.getComputerCase().pressPowerButton();
+    }
 }
