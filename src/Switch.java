d4034e52f6b92dbf123c86f30e472421483c4d0f
gagan <gasingla07@gmail.com>
2025-06-12 11:33:02 -0500

    traditional switch

--- src/Switch.java
+++ src/Switch.java
@@ -1,60 +1,28 @@
+import java.io.IOException;
+
 public class Switch {
     public static void main(String[] args) {
-        int switchValue = 3;
+
+        int switchValue = 1;
 
         switch (switchValue) {
-            case 1 -> System.out.println("Value 1");
-            case 2 -> System.out.println("Value 2");
-            case 3, 4, 5 -> {
-                System.out.println("Was 3 or 4 or 5");
+            case 1 :
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3 : case 4 : case 5:
+                System.out.println("Was a 3, or 4 or 5");
                 System.out.println("Actually it was a " + switchValue);
-            }
-            default -> System.out.println("Was not between 1 and 5");
-        }
-
-        String month = "JANUAR";
-        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
-
-        natoWords('B');
-
-        printDayOfWeek(0);
-
-//        int day = 0;
-//        String dayOfTheWeek = printDayOfWeek(day);
-
-    }
-
-    public static String getQuarter(String month) {
-        return switch (month) {
-            case "JANUARY" -> {yield "1st";}
-            default -> {
-                String badResponse = month + " is bad";
-                //return badresponse;
-                // return cannot be used inside a Enhanced switch
-                yield badResponse;
-            }
-        };
-    }
-
-    public static void natoWords(char ch){
-        switch (ch) {
-            case 'A':
-                System.out.println("Char " + ch + ":" + " Able");
                 break;
             default:
-                System.out.println("Letter not found");
+                System.out.println("Was not 1 or 2");
+                break;
         }
-    }
+        // More code here
 
-    public static void printDayOfWeek(int day) {
-        String dayOfWeek = switch (day) {
-          case 0 -> "Sunday";
-          case 1 -> "Monday";
-          case 2 -> "Tuesday";
-          case 3 -> {yield "Wednesday";}
-          default -> "Invalid Day";
-        };
-        System.out.println(day + " of the week is " + dayOfWeek);
+
     }
 
 }
--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,84 +1,10 @@
 package dev.lpa;
 
-import java.util.Arrays;
-import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] firstArray = getRandomArray(10);
-        System.out.println(Arrays.toString(firstArray));
-        Arrays.sort(firstArray);
-        System.out.println(Arrays.toString(firstArray));
-
-        int[] secondArray = new int[10];
-        System.out.println(Arrays.toString(secondArray));
-        Arrays.fill(secondArray, 5);
-        System.out.println(Arrays.toString(secondArray));
-
-        int[] thirdArray = getRandomArray(10);
-        System.out.println(Arrays.toString(thirdArray));
-
-        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
-        System.out.println(Arrays.toString(fourthArray));
-
-        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
-        Arrays.sort(sArray);
-        System.out.println(Arrays.toString(sArray));
-        if (Arrays.binarySearch(sArray, "Mark") >= 0) {
-            System.out.println("found");
-        }
-        
-    }
-
-//        int[] myIntArray  = new int[10];
-////        myIntArray[0] = 45.0;
-//
-//        myIntArray[5] = 50;
-//
-//        double [] myDoubleArray = new double[10];
-//        myDoubleArray[2] = 3.5;
-//
-//        System.out.println(myDoubleArray[2]);
-//        int[] firsTen = {1,2,3,4,5,6,7,8,9,10};
-//
-//        System.out.println("first = " + firsTen[0]);
-//        int arrayLength = firsTen.length;
-//        System.out.println("length of array " + arrayLength);
-//
-//        System.out.println("last = " + firsTen[arrayLength - 1]);
-//
-//        int[] newArray = new int[5];
-//        //newArray = new int[] {1,2,3,4,5};
-//
-//        for(int element: newArray) {
-//            System.out.println(element + " ");
-//        }
-//
-//        System.out.println();
-//        System.out.println(Arrays.toString(newArray));
-//
-//        Object objectVariable = newArray;
-//        if(objectVariable instanceof int[]) {
-//            System.out.println("ObjectVariable is really an int array");
-//        }
-//
-//        String[] names = {"Gagan"};
-//        System.out.println(names);
-
-
-
-
-    private static int[] getRandomArray(int len) {
-        Random random = new Random();
-        int[] newInt = new int[len];
-        for(int i = 0; i < newInt.length; i++) {
-            newInt[i] = random.nextInt(100);
-        }
-//        for(int x: newInt) {
-//            x = random.nextInt(100);
-//        }
-
-        return newInt;
+        System.out.println();
     }
 }
 
