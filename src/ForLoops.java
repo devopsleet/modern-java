7d521c1bf0de7c74e57d2ad0e5762e29952b21e4
gagan <gasingla07@gmail.com>
2025-06-13 16:06:52 -0500

    example program on the for loop

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -11,8 +11,24 @@ public class ForLoops {
 
     public static double calculateInterest(double amount, double interestRate) {
         //System.out.println("This would be calculateInterest");
+        //System.out.println("calculate the real values and the interest of he ");
+        //System.out.println();
+        System.out.println("class");
+        System.out.println("private");
+        System.out.println("class2");
+        System.out.println("It's the Java Language");
+        System.out.println("To calculate the percentage of the interest rate ");
+        System.out.println();
+        System.out.println("Consignor SKU");
+        System.out.println();
+        System.out.println();
 
         return (amount * (interestRate/ 100));
+
+
+
+
+
 
 
     }
