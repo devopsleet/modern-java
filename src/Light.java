2a34f708a24e117c7dacacd4116f775ac5b18518
gagan <gasingla07@gmail.com>
2026-08-21 14:51:15 -0500

    long data type

---
+++ src/Light.java
@@ -0,0 +1,15 @@
+public class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days, seconds, distance;
+
+        lightspeed = 186000;
+        days = 1000;
+
+        distance = days * 24 * 60 * 60;
+
+        System.out.print("In " + days );
+        System.out.print(" days light will travel about ");
+        System.out.print(distance + " miles");
+    }
+}
