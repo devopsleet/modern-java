7405b8c2bbe3fbbe2796d71520d3c8437823381e
gagan <gasingla07@gmail.com>
2026-08-30 22:54:58 -0500

    posting challenges

---
+++ src/Challenges/Challenge1.java
@@ -0,0 +1,15 @@
+package Challenges;
+
+public class Challenge1 {
+
+    public static void main(String[] args) {
+
+        int pounds = 5;
+
+        float kilos = 5 * (0.45359237f);
+
+        System.out.println("The kilos are " + kilos);
+
+    }
+
+}
--- src/Example2.java
+++ src/Example2.java
@@ -6,16 +6,32 @@ class Example2 {
         System.out.println(Double.MIN_VALUE);
         System.out.println(Double.MAX_VALUE);
 
-        int myIntValue = 5; float myFloatValue = 5; double myDoubleValue = 5;
+        int myIntValue = 5; float myFloatValue = 50_000_000.0f; double myDoubleValue = 5d;
         System.out.println(myIntValue);
         System.out.println(myFloatValue);
 
         myFloatValue = 5f;
         myDoubleValue = 5d;
 
-        // By default, real numbers are double type
-        float myOtherFloatValue = (float) 5.25;
-        System.out.println(myOtherFloatValue);
+        myIntValue = 5/3;
+        System.out.println(myIntValue);
+
+        myFloatValue = 5f/3f;
+        System.out.println(myFloatValue);
+
+
+        myDoubleValue = 5d/3d;
+        System.out.println(myDoubleValue);
+
+        myDoubleValue = 5.0/3.0;
+        System.out.println(myDoubleValue);
+
+        myFloatValue = (float) (5.0/3.0);
+        System.out.println(myFloatValue);
+
+
+
+
 
     }
 }
