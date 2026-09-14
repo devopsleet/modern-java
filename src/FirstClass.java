046794110a8675d72a58418c18544be48a075cad
gagan <gasingla07@gmail.com>
2026-09-14 11:42:24 -0500

    character data type

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -11,6 +11,13 @@ public class FirstClass {
         pi = 3.1416;
         a = pi *  r * r;
 
+        // character type
+        char ch1 = 'X';
+        System.out.println("chi contains " + ch1);
+
+        ch1++;
+        System.out.println("ch1 is now " + ch1);
+
         System.out.println("Area of circle is " + a);
     }
 }
