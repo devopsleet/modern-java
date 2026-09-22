e450921d9f557c2d49fa896b38d65350af2f0367
gagan <gasingla07@gmail.com>
2026-09-22 05:07:26 -0500

    switch statement

--- src/Demo.java
+++ src/Demo.java
@@ -2,12 +2,22 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = 1;
 
-        // ternary
+        switch(n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Sunday");
 
-        System.out.println(n%2==0?"even": "odd");
-
+        }
 
 
     }
