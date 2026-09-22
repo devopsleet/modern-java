848400ed05e74d4f493db010f86c6903a7983fb7
gagan <gasingla07@gmail.com>
2026-09-22 04:57:36 -0500

    conditional statement

--- src/Demo.java
+++ src/Demo.java
@@ -2,13 +2,16 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        int x = 7;
-        int y = 5;
-        int a = 5;
-        int b = 9;
+        int x = 8;
+        int y = 7;
+        int z = 6;
 
-        boolean result = a > b;
-        System.out.println(!result);
+        if(x > y && x > z) {
+            System.out.println(x);
+        } else {
+            System.out.println(y);
+        }
+
 
 
     }
