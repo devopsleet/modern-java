eb01ed56e585a632cedd7bf204c91de8096c2e41
gagan <gasingla07@gmail.com>
2026-09-22 05:31:37 -0500

    do while loop

--- src/Demo.java
+++ src/Demo.java
@@ -2,10 +2,11 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        while(i< 5) {
-            System.out.println("Hi");
+        int i = 5;
+
+        do {
+            System.out.println("Hi " + i);
             i++;
-        }
+        }while(i<=4);
     }
 }
