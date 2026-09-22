fe1756f786f01f8c41d766ace2e81a6aab32ab1f
gagan <gasingla07@gmail.com>
2026-09-22 04:59:03 -0500

    ternary operator

--- src/Demo.java
+++ src/Demo.java
@@ -2,15 +2,11 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        int x = 8;
-        int y = 7;
-        int z = 6;
+        int n = 5;
 
-        if(x > y && x > z) {
-            System.out.println(x);
-        } else {
-            System.out.println(y);
-        }
+        // ternary
+
+        System.out.println(n%2==0?"even": "odd");
 
 
 
