674501fda2f3a268ed48f55596cbfc7a57b77096
gagan <gasingla07@gmail.com>
2026-09-24 14:06:59 -0500

    improved stack class

--- src/Stack.java
+++ src/Stack.java
@@ -1,30 +1,53 @@
-// This class defines an integer stack that can hold 10 values
+// Improved stack class that uses the length array member
+
+class TestStack2 {
+    // instance variables
+    private int[] stck;
+    private int tos;
+
+    // Constructor Initialization
+    TestStack2(int size) {
+        stck = new int[size];
+        this.tos = -1;
+    }
+
+    void push(int element) {
+        if (tos == stck.length-1) {
+            System.out.println("Stack is full");
+        } else {
+            stck[++tos] = element;
+        }
+    }
+
+    int pop() {
+        if (tos == -1) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else {
+            return stck[tos--];
+        }
+    }
+
+}
 
 class Stack {
-    int[] stck = new int[10];
-    int tos;
+    public static void main(String[] args) {
 
-    // constructor
-    Stack() {
-        tos = -1;
+        TestStack2 mystack1 = new TestStack2(10);
+        TestStack2 mystack2 = new TestStack2(5);
+
+        for(int i = 0; i < 10; i++) {
+            mystack1.push(i);
+        }
+
+        for(int j = 0; j < 5; j++) {
+            mystack2.push(j);
+        }
+
+        System.out.println("Stack in mystack2");
+        for(int i = 0; i < 5; i++) {
+            System.out.println(mystack2.pop());
+        }
+
     }
-
-    // Push an item onto the stack
-    void push(int item) {
-        if (tos == 9)
-            System.out.println("Stack is full");
-        else
-            stck[++tos] = item;
-    }
-
-    // Pop an item from the stack
-    int pop() {
-        if (tos < 0) {
-            System.out.println("Stack undeflow");
-            return 0;
-        } else
-            return stck[tos--];
-    }
-
-
 }
